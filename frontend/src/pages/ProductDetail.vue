f195917f1e7144408d07dd439c3ff82894d8e86d
Chenyang Li (Sean) <cli049@uottawa.ca>
2024-12-14 00:26:54 +0000

    Updated detail page

--- frontend/src/pages/ProductDetail.vue
+++ frontend/src/pages/ProductDetail.vue
@@ -1,9 +1,22 @@
 <template>
-  <h1>Product Detail</h1>
+  <div class="img-wrap">
+    <img :src="product.imageName" />
+  </div>
+  <div class="product-details">
+    <h1>{{ product.name }}</h1>
+    <h3 class="price">{{ product.price }}</h3>
+    <button class="add-to-cart">Add to cart</button>
+  </div>
 </template>
 
 <script>
+import { products } from "../data";
 export default {
   name: "PrudctDetail",
-}
+  data() {
+    return {
+      product: products.find((p) => p.id === this.$route.params.productId),
+    };
+  },
+};
 </script>
