fa8854bfa80a9ba8bb67c700429689a69fe204d6
Chenyang Li (Sean) <cli049@uottawa.ca>
2024-12-14 14:09:00 +0000

    Added NavBar

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <router-link to="/products" class="products-link">
+    <div class="nar-bar">
+      <div class="logo-wrap">
+        <img :src="logo" />
+      </div>
+    </div>
+  </router-link>
+  <router-link to="/cart" class="cart-link">
+    <button class="cart">Shopping Cart</button>
+  </router-link>
+</template>
+
+<script>
+import logo from "@/assets/logo-hexagon.svg";
+export default {
+  name: "NavBar",
+  data() {
+    return {
+      logo,
+    };
+  },
+};
+</script>
