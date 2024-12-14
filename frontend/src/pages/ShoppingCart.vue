13d1484b24cf612f775b4ef7251b62c3846dfeea
Chenyang Li (Sean) <cli049@uottawa.ca>
2024-12-14 13:59:12 +0000

    Updated cart

--- frontend/src/pages/ShoppingCart.vue
+++ frontend/src/pages/ShoppingCart.vue
@@ -1,9 +1,27 @@
 <template>
   <h1>Shopping Cart</h1>
+  <div v-if="cartItems.length > 0">
+    <div class="product-container" v-for="product in cartItems" :key="product.id">
+      <img class="product-image" :src="product.imageName" />
+      <div class="details-wrap">
+        <h3>{{ product.name }}</h3>
+        <p>{{ product.price }}</p>
+      </div>
+      <button class="remove-button">Remove from Cart</button>
+    </div>
+    <button class="checkout-button">Proceed to Checkout</button>
+  </div>
+  <div v-if="cartItems.length === 0">You currently have no items in your cart!</div>
 </template>
 
 <script>
+import { cartItems } from "../data";
 export default {
   name: "ShoppingCart",
-}
+  data() {
+    return {
+      cartItems,
+    };
+  },
+};
 </script>
